df320e8be6c4b945fa0af993a6e56089619c6c25
Larissa Leonel da Silva <56083152+Larissaleo@users.noreply.github.com>
2022-01-26 14:49:14 -0300

    Add files via upload
    Código para gerar numero aleatório

---
+++ NumAleatorio/NumAleatorio/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program{
+
+    static void Main(string[] args)
+    {
+    Random NumAleatorio = new Random();
+
+    int NumGerado = NumAleatorio.Next(0,7);
+
+    int ValorUser;
+    System.Console.WriteLine("Tente adivinhar o número gerado: ");
+    ValorUser = int.Parse(Console.ReadLine());
+
+    while(ValorUser != NumGerado){
+        System.Console.WriteLine(":( Esse não foi o número gerado. Tente novamente");
+        ValorUser = int.Parse(Console.ReadLine());
+
+
+    }
+        if(ValorUser == NumGerado){
+        System.Console.WriteLine($"Parabéns você acertou o numero gerado foi {NumGerado}");
+        
+        }
+    
+
+
+    }
+    
+
+
+}
