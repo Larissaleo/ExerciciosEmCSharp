8ab13870ee0cc9767e8b915e41065888d17cf0b0
Larissa Leonel da Silva <56083152+Larissaleo@users.noreply.github.com>
2022-01-28 19:30:09 -0300

    Update Program.cs
    atualização de arquivos

--- olimpiadas/Olimpiada/Program.cs
+++ olimpiadas/Olimpiada/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Olimpiada.src.Entities;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,51 +9,37 @@ namespace Olimpiada
     class Program
     {
         
-        
+         static List<Atleta> atletas = new List<Atleta>();    
         static void Main(string[] args)
         {
-            int i;
-
+            int i=0, j=1;
+           
             Console.WriteLine("Atletas:");
+
+            while(i<3)
+            {   
+                
+
+                atletas.Add(new Atleta(" "," ", 0));
+
+                Console.WriteLine($"Digite o NOME do atleta  {j} ");
+                atletas[i].Nome = Console.ReadLine();
+                Console.WriteLine($"Digite o PAÍS do atleta {j}: ");
+                atletas[i].Pais = Console.ReadLine();
+                Console.WriteLine($"Digite o RESULTADO (em metros) do atleta {j}: ");
+                atletas[i].Distancia = Convert.ToDouble(Console.ReadLine());
+
+                j++;
+               i++; 
+            }
          
-                for(i=1; i<=3; i++){
-                    List<Atleta> atletas = new List<Atleta>();    
-                        atletas.Add(new Atleta("x","x", 0));
-                        atletas.Add(new Atleta("x","x", 0));
-                        atletas.Add(new Atleta("x","x", 0));
+               
+                    
 
-                        
-
-                        // atletas.Add(new Atleta("x","x", 0));
-                        // atletas.Add(new Atleta("x","x", 0));
-                        // atletas.Add(new Atleta("x","x", 0));
-                        // atletas.Add(new Atleta("x","x", 0));
-                        // atletas.Add(new Atleta("x","x", 0));
-                        // atletas.Add(new Atleta("x","x", 0));
-                        // atletas.Add(new Atleta("x","x", 0))
-
-                    Console.WriteLine($"Digite o NOME do atleta : {i} ");
-                    atletas[0].Nome = Console.ReadLine();
-                    Console.WriteLine($"Digite o PAÍS do atleta {i}: ");
-                    atletas[0].Pais = Console.ReadLine();
-                    Console.WriteLine($"Digite o RESULTADO (em metros) do atleta {i}: ");
-                    atletas[0].Distancia = double.Parse(Console.ReadLine());
-                   
-                   }
-
-
-                    Console.WriteLine("");
-                    Console.WriteLine("Ordenação das posições:");
-                    Console.WriteLine("");
-                    Console.WriteLine("");
-
-
-                     var podium = atletas.OrderBy(atletas => atletas.Distancia);  
-                    foreach(var a in podium)
-                    {
-                    Console.WriteLine("{0} {1} {2}", a.Nome, a.Pais, a.Distancia);
-                    }   
-                           
+                            
+               
+                    
+                     
                     
            
         }   
