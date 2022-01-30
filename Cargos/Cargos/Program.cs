353a3b90738649a6bc870dfc99413d9f8cf02675
Larissa Leonel da Silva <56083152+Larissaleo@users.noreply.github.com>
2022-01-29 22:56:19 -0300

    Update Program.cs
    Melhorias do código

--- Cargos/Cargos/Program.cs
+++ Cargos/Cargos/Program.cs
@@ -7,57 +7,37 @@ namespace Cargos
 {
     class Program
     {
+
+        static List<Pessoa> pessoa = new List<Pessoa>();
         static void Main(string[] args)
-        {
+         {
             Console.WriteLine("Controle de funcionarios");
 
 
-            List<Pessoa> pessoa = new List<Pessoa>();
-                int Lenght_pessoa = pessoa.Count;
-                pessoa.Add(new Pessoa(" " , " "));
-                pessoa.Add(new Pessoa(" " , " "));
-                pessoa.Add(new Pessoa(" " , " "));
-                pessoa.Add(new Pessoa(" " , " "));
-                pessoa.Add(new Pessoa(" " , " "));
+                int i=0, j=1;
+                while(i<5){
 
+                    pessoa.Add(new Pessoa(" " , " "));
+
+                    Console.WriteLine($"Informe o nome da {j} pessoa: ");
+                    pessoa[i].Nome=Console.ReadLine();
+                    Console.WriteLine($"Informe o cargo da {j} pessoa (Starter, Junior ou Sênior): ");
+                    pessoa[i].Cargo=Console.ReadLine();
+
+                    i++;
+                    j++;
+                    }
             
+                    int Lenght_pessoa = pessoa.Count;
 
-                Console.WriteLine("Informe o nome da 1ª pessoa: ");
-                pessoa[0].Nome=Console.ReadLine();
-                Console.WriteLine("Informe o cargo da 1ª pessoa (Starter, Junior ou Sênior): ");
-                pessoa[0].Cargo=Console.ReadLine();
-
-                Console.WriteLine("Informe o nome da 2ª pessoa: ");
-                pessoa[1].Nome=Console.ReadLine();
-                Console.WriteLine("Informe o cargo da 2ª pessoa (Starter, Junior ou Sênior): ");
-                pessoa[1].Cargo=Console.ReadLine();
-
-                Console.WriteLine("Informe o nome da 3ª pessoa: ");
-                pessoa[2].Nome=Console.ReadLine();
-                Console.WriteLine("Informe o cargo da 3ª pessoa (Starter, Junior ou Sênior): ");
-                pessoa[2].Cargo=Console.ReadLine();
-
-                Console.WriteLine("Informe o nome da 4ª pessoa: ");
-                pessoa[3].Nome=Console.ReadLine();
-                Console.WriteLine("Informe o cargo da 4ª pessoa (Starter, Junior ou Sênior): ");
-                pessoa[3].Cargo=Console.ReadLine();
-
-                Console.WriteLine("Informe o nome da 5ª pessoa: ");
-                pessoa[4].Nome=Console.ReadLine();
-                Console.WriteLine("Informe o cargo da 5ª pessoa (Starter, Junior ou Sênior): ");
-                pessoa[4].Cargo=Console.ReadLine();
-
-                    int i, qtdStarter = 0, qtdJunior=0, qtdSenior=0;
+                    int z, qtdStarter = 0, qtdJunior=0, qtdSenior=0;
 
                     for(i=0; i<pessoa.Count; i++){
 
                         
                         if (pessoa[i].Cargo.ToString()  == "Starter" )
-                        //||pessoa[i].Cargo.ToString() == "starter" || pessoa[i].Cargo.ToString() == "STARTER")
                         {
                             qtdStarter++;
-
-                            
                         }
 
                         if(pessoa[i].Cargo.ToString()  == "Junior"){
@@ -76,11 +56,11 @@ namespace Cargos
                             qtdSenior++;
                         }
 
-            }
-                    Console.WriteLine("Total de pessoas");
-                    Console.WriteLine($"Starter: {qtdStarter}");
-                    Console.WriteLine($"Junior: {qtdJunior}");
-                    Console.WriteLine($"Sênior: {qtdSenior}");
+
+                        Console.WriteLine("Total de pessoas");
+                        Console.WriteLine($"Starter: {qtdStarter}");
+            }           Console.WriteLine($"Junior: {qtdJunior}");
+                        Console.WriteLine($"Sênior: {qtdSenior}");
 
                 
                 
